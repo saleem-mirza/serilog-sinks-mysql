bd8d6252210185e33a8c63428daf9a7a5c7cc31b
Saleem Mirza <saleem-mirza@users.noreply.github.com>
2016-09-24 23:01:58 -0400

    Initial code commit for MySQL sink

---
+++ src/serilog-sinks-mysql/LoggerConfigurationMySQLExtensions.cs
@@ -0,0 +1,68 @@
+﻿// Copyright 2016 Serilog Contributors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Serilog.Debugging;
+
+namespace Serilog
+{
+    using System;
+    using Serilog.Configuration;
+    using Serilog.Events;
+    using Serilog.Sinks.MySQL;
+
+    /// <summary>
+    ///     Adds the WriteTo.SQLite() extension method to <see cref="LoggerConfiguration" />.
+    /// </summary>
+    public static class LoggerConfigurationMySqlExtensions
+    {
+        /// <summary>
+        ///     Adds a sink that writes log events to a MySQL database.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="connectionString">The connection string to MySQL database.</param>
+        /// <param name="tableName">The name of the MySQL table to store log.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="storeTimestampInUtc">Store timestamp in UTC format</param>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration MySQL(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string connectionString,
+            string tableName = "Logs",
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            bool storeTimestampInUtc = false)
+        {
+            if (loggerConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(loggerConfiguration));
+            }
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            try
+            {
+                return loggerConfiguration.Sink(
+                    new MySqlSink(connectionString, tableName, storeTimestampInUtc),
+                    restrictedToMinimumLevel);
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/serilog-sinks-mysql/Sinks/MySQL/MySqlSink.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using Serilog.Events;
+using Serilog.Core;
+using Serilog.Debugging;
+using Serilog.Extensions;
+using Serilog.Sinks.Batch;
+
+namespace Serilog.Sinks.MySQL
+{
+    internal class MySqlSink : BatchProvider, ILogEventSink
+    {
+        private readonly string _connectionString;
+        private readonly string _tableName;
+        private readonly bool _storeTimestampInUtc;
+
+        public MySqlSink(string connectionString,
+            string tableName = "Logs",
+            bool storeTimestampInUtc = false) : base(500, Environment.ProcessorCount)
+        {
+            _connectionString = connectionString;
+            _tableName = tableName;
+            _storeTimestampInUtc = storeTimestampInUtc;
+
+            var sqlConnection = GetSqlConnection();
+            CreateTable(sqlConnection);
+        }
+
+        private MySqlConnection GetSqlConnection()
+        {
+            try
+            {
+                var conn = new MySqlConnection(_connectionString);
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        private MySqlCommand GetInsertCommand(MySqlConnection sqlConnection)
+        {
+            var tableCommandBuilder = new StringBuilder();
+            tableCommandBuilder.Append($"INSERT INTO  {_tableName} (");
+            tableCommandBuilder.Append("Timestamp, Level, Message, Exception, Properties) ");
+            tableCommandBuilder.Append("VALUES (@ts, @lvel, @msg, @ex, @prop)");
+
+            var cmd = sqlConnection.CreateCommand();
+            cmd.CommandText = tableCommandBuilder.ToString();
+
+            cmd.Parameters.Add(new MySqlParameter("@ts", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@lvel", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@msg", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@ex", MySqlDbType.VarChar));
+            cmd.Parameters.Add(new MySqlParameter("@prop", MySqlDbType.VarChar));
+
+            return cmd;
+        }
+
+        private void CreateTable(MySqlConnection sqlConnection)
+        {
+            try
+            {
+                var tableCommandBuilder = new StringBuilder();
+                tableCommandBuilder.Append($"CREATE TABLE IF NOT EXISTS {_tableName} (");
+                tableCommandBuilder.Append("id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,");
+                tableCommandBuilder.Append("Timestamp VARCHAR(100),");
+                tableCommandBuilder.Append("Level VARCHAR(15),");
+                tableCommandBuilder.Append("Message TEXT,");
+                tableCommandBuilder.Append("Exception TEXT,");
+                tableCommandBuilder.Append("Properties TEXT,");
+                tableCommandBuilder.Append("_ts TIMESTAMP)");
+
+                var cmd = sqlConnection.CreateCommand();
+                cmd.CommandText = tableCommandBuilder.ToString();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+            }
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            PushEvent(logEvent);
+        }
+
+        protected override void WriteLogEvent(ICollection<LogEvent> logEventsBatch)
+        {
+            try
+            {
+                using (var sqlCon = GetSqlConnection())
+                {
+                    using (var tr = sqlCon.BeginTransaction())
+                    {
+                        var insertCommand = GetInsertCommand(sqlCon);
+                        insertCommand.Transaction = tr;
+
+                        foreach (var logEvent in logEventsBatch)
+                        {
+                            insertCommand.Parameters["@ts"].Value = _storeTimestampInUtc
+                                ? logEvent.Timestamp.ToUniversalTime().ToString()
+                                : logEvent.Timestamp.ToString();
+
+                            insertCommand.Parameters["@lvel"].Value = logEvent.Level.ToString();
+                            insertCommand.Parameters["@msg"].Value = logEvent.MessageTemplate.ToString();
+                            insertCommand.Parameters["@ex"].Value = logEvent.Exception?.ToString();
+                            insertCommand.Parameters["@prop"].Value = logEvent.Properties.Json();
+
+                            insertCommand.ExecuteNonQuery();
+                        }
+                        tr.Commit();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SelfLog.WriteLine(ex.Message);
+            }
+        }
+    }
+}
